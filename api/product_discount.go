df4208bf106a76334a7394cf7a3b4784552f5acc
Nathan03nable <nathan.pringle@n-able.com>
2024-05-31 14:25:51 +0100

    Add product discount

---
+++ api/product_discount.go
@@ -0,0 +1,47 @@
+package handler
+
+import (
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"price-control/api/generated"
+	"price-control/pkg/db"
+)
+
+func SetProductDiscount(w http.ResponseWriter, request *http.Request) {
+	var product generated.PostProductDiscountJSONBody
+
+	err := json.NewDecoder(request.Body).Decode(&product)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	dbConnection, err := db.ConnectToDb()
+	if err != nil {
+		_, _ = fmt.Fprintf(w, "<h1>Error connecting to database: %v(</h1>", err)
+		return
+	}
+
+	defer func(dbConnection *sql.DB) {
+		_ = dbConnection.Close()
+	}(dbConnection)
+
+	query := "UPDATE products SET discount = $1 WHERE name = $2"
+	fmt.Println(query)
+	_, err = dbConnection.Exec(query, product.DiscountType, product.ProductName)
+	if err != nil {
+		_, _ = fmt.Fprintf(w, "<h1>Error inserting product discount: </h1>")
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	w.Header().Set("Content-Type", "application/json")
+	_, _ = w.Write([]byte(`{"message": "Product price added successfully"}`))
+}
+
+func init() {
+	http.HandleFunc("/productDiscount", SetProductDiscount)
+}
