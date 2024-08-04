3ac5b0d3525161d65381420ec1af0a491c5e4723
Nathan03nable <nathan.pringle@n-able.com>
2024-08-04 16:17:07 +0100

    Add headers

--- api/product_price.go
+++ api/product_price.go
@@ -43,7 +43,7 @@ func SetProductPrice(w http.ResponseWriter, request *http.Request) {
 	}
 
 	w.Header().Set("Content-Type", "application/json")
-	w.Header().Set("Access-Control-Allow-Origin", "http://localhost:4200")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
 	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 	w.WriteHeader(http.StatusOK)
